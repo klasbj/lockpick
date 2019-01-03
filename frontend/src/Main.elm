809ee525feba9cf0f7b99390ca030385a43ef604
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2019-01-04 00:10:32 +0100

    Run elm-format.

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,40 +1,63 @@
 module Main exposing (main)
+
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
 
+
+
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
 init =
     0
 
+
+
 -- UPDATE
 
-type Msg = Reset | Increment
+
+type Msg
+    = Reset
+    | Increment
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Reset -> 0
-        Increment -> model + 1
+        Reset ->
+            0
+
+        Increment ->
+            model + 1
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-    div [] [
-          p [] [text ("woo " ++ String.fromInt model)]
-        , button [onClick Increment] [text "+"]
-        , button [onClick Reset] [text "Reset"]
-    ]
+    div []
+        [ p [] [ text ("woo " ++ String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Reset ] [ text "Reset" ]
+        ]
+
+
 
 -- SUBS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
 
 main =
     Browser.sandbox
