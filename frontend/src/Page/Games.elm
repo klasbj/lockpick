733318f6831672e81b1117d0bcb955a85fcc9a78
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2019-01-06 00:23:27 +0100

    Simple games display.

--- frontend/src/Page/Games.elm
+++ frontend/src/Page/Games.elm
@@ -1,7 +1,9 @@
-module Page.Games exposing (Model, Msg, toSession, init, update, view)
+module Page.Games exposing (Model, Msg, init, toSession, update, view)
 
 import Html exposing (..)
+import Html.Attributes as Attr
 import Session exposing (Session)
+import Task
 
 
 
@@ -10,7 +12,48 @@ import Session exposing (Session)
 
 type alias Model =
     { session : Session
+    , games : List Game
     }
+
+
+type alias Game =
+    { firstPlayer : Player
+    , secondPlayer : Player
+    , winner : Winner
+    }
+
+
+type alias Player =
+    { playerName : String
+    , deck : Deck
+    , chains : Int
+    }
+
+
+type alias Deck =
+    { name : String
+    , id : String
+    , houses : Houses
+    }
+
+
+type Houses
+    = Houses House House House
+
+
+type House
+    = Dis
+    | Sanctum
+    | Brobnar
+    | Logos
+    | Mars
+    | Untamed
+    | Shadows
+
+
+type Winner
+    = FirstPlayer
+    | SecondPlayer
 
 
 toSession : Model -> Session
@@ -20,7 +63,7 @@ toSession m =
 
 init : Session -> ( Model, Cmd Msg )
 init session =
-    ( { session = session }, Cmd.none )
+    ( { session = session, games = [] }, Task.perform identity <| Task.succeed LoadGames )
 
 
 
@@ -28,12 +71,42 @@ init session =
 
 
 type Msg
-    = None
+    = LoadGames
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        LoadGames ->
+            ( { model
+                | games =
+                    [ { firstPlayer = player "a" "deckA" "123" Dis Sanctum Mars 0
+                      , secondPlayer = player "b" "deckB" "543" Mars Brobnar Shadows 0
+                      , winner = SecondPlayer
+                      }
+                    , { firstPlayer = player "a" "deckB" "543" Mars Brobnar Shadows 0
+                      , secondPlayer = player "b" "deckA" "123" Dis Sanctum Mars 0
+                      , winner = FirstPlayer
+                      }
+                    , { firstPlayer = player "b" "deckB" "543" Mars Brobnar Shadows 12
+                      , secondPlayer = player "a" "deckA" "123" Dis Sanctum Mars 0
+                      , winner = FirstPlayer
+                      }
+                    ]
+              }
+            , Cmd.none
+            )
+
+
+player name deckName deckId h1 h2 h3 chains =
+    { playerName = name
+    , deck =
+        { name = deckName
+        , id = deckId
+        , houses = Houses h1 h2 h3
+        }
+    , chains = chains
+    }
 
 
 
@@ -41,7 +114,36 @@ update _ model =
 
 
 view : Model -> { title : String, content : Html Msg }
-view _ =
+view model =
     { title = "Games"
-    , content = div [] [ p [] [ text "Games all around" ] ]
+    , content =
+        div []
+            [ p [] [ text "Games all around" ]
+            , viewGameTable model.games
+            ]
     }
+
+
+viewGameTable : List Game -> Html Msg
+viewGameTable games =
+    div [ Attr.class "gametable" ]
+        [ table []
+            ((tr [] <| List.map headerCell [ "Player", "Deck", "Chains", "vs", "Player", "Deck", "Chains" ]) :: List.map gameRow games)
+        ]
+
+
+headerCell s =
+    th [] [ text s ]
+
+
+gameRow { firstPlayer, secondPlayer, winner } =
+    tr [] <| playerAndDeck firstPlayer (winner == FirstPlayer) ++ [td [] []] ++ playerAndDeck secondPlayer (winner == SecondPlayer)
+
+
+playerAndDeck : Player -> Bool -> List (Html Msg)
+playerAndDeck { playerName, deck, chains } isWinner =
+    let
+        cell s =
+            td [ Attr.classList [ ("game-result", True), ( "winner", isWinner ) ] ] [ text s ]
+    in
+    [ cell playerName, cell deck.name, cell (String.fromInt chains) ]
